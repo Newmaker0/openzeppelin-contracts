69d00d85078bd4939085242e49d2f2ca71a4e2f4
Pedro Wilson <pedrowilsonsilvaff@gmail.com>
2023-10-27 13:51:02 +0000

    Create contract-w-constructor.sol

---
+++ contracts/contract-w-constructor.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Sample{
+    string public publicStr;
+    string private privateStr;
+    string internal internalStr;
+    uint256 public publicUint;
+    uint256 private privateUint;
+    uint256 internal internalUint;
+
+
+
+}
