a77745a2b4afc39fc8f5ce9140e123a467faced6
Pedro Wilson <pedrowilsonsilvaff@gmail.com>
2023-11-21 12:28:24 -0300

    Update test.sol

--- contracts/test.sol
+++ contracts/test.sol
@@ -1,31 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
 
-contract SimpleCounter {
-    int256 private count;
-
-    // Event that will be emitted whenever the counter is updated
-    event CountUpdated(int256 newCount);
-
-    // Initialize the counter with an initial value of 0
-    constructor() {
-        count = 0;
-    }
-
-    // Function to increment the counter by 1
-    function increment() public {
-        count += 1;
-        emit CountUpdated(count);
-    }
-
-    // Function to decrement the counter by 1
-    function decrement() public {
-        count -= 1;
-        emit CountUpdated(count);
-    }
-
-    // Function to get the current counter value
-    function getCount() public view returns (int256) {
-        return count;
-    }
+contract Day {
+    string public day = "day";
 }
