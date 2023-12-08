fd5d879c76f5ead96aaf3dfe529a7c2580ffa359
Pedro Wilson <pedrowilsonsilvaff@gmail.com>
2023-12-07 21:47:26 -0300

    Update test.sol

--- contracts/test.sol
+++ contracts/test.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
-contract Day {
-    string public day = "day";
+contract SimpleStorage {
+    uint256 private storedData;
+
+    function set(uint256 x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint256) {
+        return storedData;
+    }
 }
